51dc38027ec4e86824b7987e0e518529edc67515
amandwivedi07 <aman@heynoah.ai>
2026-01-29 18:19:01 +0530

    fixes api

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const backendBaseUrl = "http://localhost:8080"
-  // process.env.NEXT_PUBLIC_API_URL ??
-  // (process.env.NODE_ENV === "development"
-  //   ? "http://localhost:8080"
-  //   : "https://discovr-backend.onrender.com")
+const backendBaseUrl =
+  process.env.NEXT_PUBLIC_API_URL ??
+  (process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : "https://discovr-backend.onrender.com")
 
 const nextConfig: NextConfig = {
   async rewrites() {
